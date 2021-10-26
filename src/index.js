2026baec7adb60429442df06a4c691965480799a
Pedro <plsg2020.ufmg@gmail.com>
2021-10-26 19:50:32 -0300

    Familiarizando com o node.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 
+//Porta em que o backend roda
 const port = process.env.PORT || 3333; 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/' , (request, response) => {
+  return response.json({
+    evento: "Semana Omni Stack",
+    aluno: "eu"
+  })
+})
 
 app.listen(port, () => {
   console.log('Server listening on port ' + port);
