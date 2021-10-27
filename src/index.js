a6865b446d319a2a03504646431e97776fb41623
Pedro <plsg2020.ufmg@gmail.com>
2021-10-27 12:00:41 -0300

    Criado o model da tabela 'Clientes'

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-const express = require('express');
+const express = require('express'); //Importa express e cors
 const cors = require('cors');
 const routes= require('./routes')
 
-//Porta em que o backend roda
+//Define a porta em que o backend roda
 const port = process.env.PORT || 3333; 
 
+//Define o uso do cors, express e json
 const app = express();
 
-app.use(cors());
+app.use(cors());  
 app.use(express.json());
 
 /**
@@ -52,6 +53,7 @@ app.use(express.json());
 
 app.use(routes);
 
+//Começa a aplicação
 app.listen(port, () => {
   console.log('Server listening on port ' + port);
 })
--- src/models/Clientes.js
+++ src/models/Clientes.js
@@ -1,1 +1,53 @@
 const connection = require('../database/connection');
+
+/**
+ * Create
+ * Read
+ * Update
+ * Delete
+ */
+
+
+module.exports = {  
+
+  //Insere um novo cliente na coluna
+  async create(cliente){
+    const result = await connection('clientes').insert(cliente);
+    return result;
+    /**
+      * 'cliente' eh um objeto JSON que estah sendo inserido na tabela, 
+      * nele tem todos os campos das colunas da tabela 'clientes' 
+      */
+  },
+
+  //Acha e retorna o cliente pelo seu id (pode ser passado mais de um parametro por props na funcao)
+  async getById(cliente_id){
+    const result = await connection('clientes')
+      .where({cliente_id : cliente_id})
+      .select('*');
+    //O where precisa especificar a coluna e o valor que se procura nela
+    //O select define quais colunas desse cliente vc quer receber
+
+    return result;
+  },
+
+  //Altera o valor de um cliente, especificado pela sua id
+  async updateById(cliente_id, cliente){
+    const result = await connection('clientes')
+      .where({cliente_id}) //{cliente_id : cliente_id} === {cliente_id}
+      .update(cliente); //Novamente, cliente eh um objeto json
+
+    return result;
+  },
+
+  //Deleta um cliente, especificado pela sua id
+  async deleteById(cliente_id){
+    const result = await connection('clientes').where({category_id}).delete();
+    return result;
+  }
+
+
+
+
+}
+
